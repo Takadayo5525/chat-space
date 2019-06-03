560d3caaf896f87a64a8d6538bc1e339c3f9e371
taka <tattaka.ta.dayo1234@gmail.com>
2019-06-03 21:22:38 +0900

    Asynchronous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+    var image = ""
+    if(message.image !== null){
+      image = `<img src = ${message.image} class ='lower-message__image'></img>`
+    }
+    var html =`<div class="message__block" data-message-id=${message.id}>
+    <div class="message__block__information">
+      <div class="message__block__information__user-name">
+        ${message.user_name}
+      </div>
+      <div class="message__block__information__date">
+        ${message.date}
+      </div>
+    </div>
+    <p class="message__block__comment">
+        ${message.content}
+    </p>
+    ${image}  
+  </div>`
+
+    return html;
+  }
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-message').append(html)
+      $('#message_content').val('')
+      $('.main-message').animate({scrollTop: $('.main-message')[0].scrollHeight}); 
+    })
+    .fail(function(){
+      alert('error');
+    })
+    return false;
+    
+  });
+})
