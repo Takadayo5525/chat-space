856558cdeb2e086d48f04a05adcc8a06b3f3d9ed
taka <tattaka.ta.dayo1234@gmail.com>
2019-06-08 17:19:44 +0900

    create.incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(name: current_user.name)
+    respond_to do |format|
+      format.json
+    end
+  end
+
+
   def edit
   end
 
